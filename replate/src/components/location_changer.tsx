379ff89703a0ff37f220dd316b6f2beec2304499
SilvanaMendozaOlarte <silvanamendozaolarte@gmail.com>
2025-03-29 18:35:47 -0400

    Create location_changer.tsx

---
+++ replate/src/components/location_changer.tsx
@@ -0,0 +1,13 @@
+// location will be a context
+const LocationChangerComponent = () => {
+    const handleLocationChange = () => {
+
+    }
+    return(
+        <button onClick={handleLocationChange}>
+            Change Location
+        </button>
+    )
+}
+
+export default LocationChangerComponent
